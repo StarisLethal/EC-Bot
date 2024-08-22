8ef23a0a63fd27e5a23cf4eee12cb1cebf56a8e5
Amenor <lethality.game@gmail.com>
2024-08-22 13:00:38 +0200

    Inital commint with spring boot project setup

---
+++ src/main/java/fr/etrangecantina/ecbot/EcBotApplication.java
@@ -0,0 +1,13 @@
+package fr.etrangecantina.ecbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/etrangecantina/ecbot/EcBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.etrangecantina.ecbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
